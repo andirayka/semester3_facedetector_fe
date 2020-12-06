b01e6669575cb2877ded108a076abdf92d580407
Andi Rayka <andi.rayka@gmail.com>
2020-12-06 10:16:43 +0700

    done video 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import Navigation from "./components/navigation/Navigation";
+import FaceRecognition from "./components/faceRecognition/FaceRecognition";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
 import Logo from "./components/logo/Logo";
 import Rank from "./components/rank/Rank";
@@ -18,14 +20,27 @@ const particleOptions = {
   },
 };
 
+const app = new Clarifai.App({ apiKey: "cc0013dd5e2c4148b941e8a364705f40" });
+
 const App = () => {
   const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
   const onInputChange = (event) => {
-    console.log(event);
+    setInput(event.target.value);
   };
 
-  const onButtonSubmit = () => {};
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, input).then(
+      (resp) => {
+        console.log(resp.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  };
 
   return (
     <div className="App">
@@ -37,6 +52,7 @@ const App = () => {
         onInputChange={onInputChange}
         onButtonSubmit={onButtonSubmit}
       />
+      <FaceRecognition imageUrl={imageUrl} />
     </div>
   );
 };
